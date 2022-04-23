4fe24c4fae734c490d3ba74191b5be2d8caab122
GEOVANYAMERGIRGIS <104278084+GEOVANYAMERGIRGIS@users.noreply.github.com>
2022-04-23 23:55:07 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+      //  Scanner scanner = new Scanner(System.in);
+     //   System.out.println("please enter the watermelon weight");
+   //     int weight = scanner.nextInt();
+//p1 P1 = new p1();
+//String result = P1.divide(weight);
+//System.out.print("The result is "+result);
+        ;
+    }
+}
---
+++ p1.java
@@ -0,0 +1,16 @@
+//public class p1 {
+
+
+
+
+
+      //  float partweight = weight /2;
+        //if(weight%2==0){
+       //     if(partweight%2 == 0){
+         //       return "yes";
+         //   }
+      //      else return "No";
+     //   }
+    //    else return "false";
+   // }
+//}
